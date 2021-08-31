59a35bccc2fcf474299b1b2b9559598dd02b1bd4
Osman TÜYSÜZ <osman.tuysuz@bil.omu.edu.tr>
2021-08-31 15:57:41 +0300

    1.1 Hello World çalışması eklendi.

---
+++ Introduction/_1HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //İlk programımız. Ekrana Merhaba Dünya yazdırmak adettendir.
+            Console.WriteLine("Merhaba Dünya");
+            Console.ReadLine();
+        }
+    }
+}

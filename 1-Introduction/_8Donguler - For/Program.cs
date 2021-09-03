f24066b00f8db5b2f293ab9c9faf2aff36297720
Osman TÜYSÜZ <osman.tuysuz@bil.omu.edu.tr>
2021-09-03 16:41:18 +0300

    1.8 Döngüler-For konusu eklendi.

---
+++ 1-Introduction/_8Donguler - For/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Donguler___For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For döngüsü bir başlangıç, bitiş ve artış miktarı olarak yazılan bir döngüdür.
+
+            //Kullanıcının girdiği sayının faktöriyelini hesaplayan program yazalım. 
+            int girilenSayi, faktoriyel = 1;
+
+            Console.Write("Pozitif bir sayı giriniz: ");
+            girilenSayi = int.Parse(Console.ReadLine());
+
+            if(girilenSayi==0)
+                Console.WriteLine("0 sayısının faktöriyeli: 1");
+            else
+            {
+                for (int i = girilenSayi; i > 0; i--)
+                {
+                    faktoriyel *= i;
+                }
+                Console.WriteLine("{0} sayısının faktöriyeli: {1}", girilenSayi, faktoriyel);
+            }
+       
+            Console.ReadLine();
+        }
+    }
+}

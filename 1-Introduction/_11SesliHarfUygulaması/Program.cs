49c289fdbfe90dca88ab19ee027cf5dcd715f200
Osman TÜYSÜZ <osman.tuysuz@bil.omu.edu.tr>
2021-09-08 18:46:40 +0300

    1.11 Sesli Harf Bulma Uygulaması eklendi.

---
+++ 1-Introduction/_11SesliHarfUygulaması/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11SesliHarfUygulaması
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sesli Harf Bulma Uygulaması
+
+            //1. Adım: Kullanıcıdan bir kelime istenmeli ve bu kelime bir değişkende tutulmalı.
+            Console.WriteLine("Kelime: ");
+            string kelime = Console.ReadLine();
+
+            char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü', 'A', 'E', 'I', 'İ', 'O', 'Ö', 'U', 'Ü' };
+
+            int sayac = 0;
+            foreach (char harf in kelime)
+            {
+                if (sesliHarfler.Contains(harf))
+                    sayac++;
+            }
+
+
+            Console.WriteLine("{0} adet sesli harf içermektedir.", sayac);
+            Console.ReadLine();
+        }
+    }
+}
